b078540e6a3269351a0861ea80ef09d6a76a2ab6
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-02-02 20:45:05 +0530

    implement president module dashbord 1

--- src/app/presidentDashboard/page.js
+++ src/app/presidentDashboard/page.js
@@ -1,43 +1,37 @@
 "use client";
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import Layout from '../presidentlayout'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Layout from "../presidentlayout";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function PresidentDashboard() {
-  const [message, setMessage] = useState('');
+  const [userName, setUserName] = useState("");
   const router = useRouter();
- 
-  useEffect(() => {
 
+  useEffect(() => {
     (async () => {
       try {
-      const content = await fetch('http://localhost:5000/api/authuser', {
-        credentials: 'include',
-    });
-    if (content.status === 200){
-      setAuth(true);
-    const data = await content.json();
-    setMessage(`Hi ${content.name}`)
-  }
-  else {
-    router.push('/login');
-  }
-}
-  catch (error) {
-    
-    setMessage('You are not logged in');
-  }
-    }
-)();
-  });
+        const content = await fetch("http://localhost:5000/api/authuser", {
+          credentials: "include",
+        });
+        if (content.status === 200) {
+          const data = await content.json();
+          setUserName(data.userName);
+        } else {
+          router.push("/login");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        console.log("You are not logged in");
+      }
+    })();
+  }, []);
 
   return (
-    <Layout >
-    <div>
-      {message}
-      <h1>President Dashboard</h1>
-      <p>Welcome to the President's exclusive dashboard!</p>
-    </div>
+    <Layout>
+          <h1 className="text-3xl font-bold text-indigo-600">
+            <Typewriter words={[`Hi , ${userName}!`]} loop={1} typeSpeed={100} />
+          </h1>
     </Layout>
   );
 }

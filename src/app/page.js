2b04a413527649867fdd4873126fd545a78761ba
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2025-04-29 02:39:57 +0530

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -71,7 +71,7 @@ export default function Login() {
                     router.push("/");
                 }
             }else{
-              router.push("/"); 
+              router.push("/directorDashboard"); 
             }
           }else {
             alert(response.data.message);
